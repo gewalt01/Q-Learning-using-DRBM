1e85bc7d9913b3035087e783db454ae6b1bb27ff
user <user@localhost>
2017-11-05 23:25:15 +0900

    多次元配列化(やっぱり実用性を考えて。速度が大事ではないし。)

--- public_html/drbm.js
+++ public_html/drbm.js
@@ -3,7 +3,7 @@
 
 /*
  * Discriminative Restricted Boltzmann Machines
- * Ising Spin of Hidden Unit: {-1, +1}
+ * Hidden Unit having Ising Spin: h_j \in {-1, +1}
  */
 function DRBM(xsize, hsize, ysize) {
     this.xsize = xsize;
@@ -17,37 +17,12 @@ function DRBM(xsize, hsize, ysize) {
     this.bias["h"] = new Float32Array(hsize);
     this.bias["y"] = new Float32Array(ysize);
     this.weight = {};
-    this.weight["xh"] = new Float32Array(xsize * hsize);
-    this.weight["hy"] = new Float32Array(hsize * ysize);
-    this.weightLength = {};
-    this.weightLength["xh"] = [xsize, hsize];
-    this.weightLength["hy"] = [hsize, ysize];
-};
-
-DRBM.prototype.getNode = function(name, index) {
-    return this.node[name][index];
-};
-
-DRBM.prototype.setNode = function(name, index, value) {
-    this.node[name][index] = value;
-};
-
-DRBM.prototype.getBias = function(name, index) {
-    return this.bias[name][index];
-};
-
-DRBM.prototype.setBias = function(name, index, value) {
-    this.bias[name][index] = value;
-};
-
-DRBM.prototype.getWeight = function(name, i, j) {
-    var index = i * this.weightLength[name][1] + j;
-    return this.weight[name][index];
-};
-
-DRBM.prototype.setWeight = function(name, i, j, value) {
-    var index = i * this.weightLength[name][1] + j;
-    this.weight[name][index] = value;
+    this.weight["xh"] = new Array(xsize);
+    for(var i = 0; i < this.weight["xh"].length; i++)
+        this.weight["xh"][i] = new Float32Array(hsize);
+    this.weight["hy"] = new Array(xsize);
+    for(var j = 0; j < this.weight["hy"].length; j++)
+        this.weight["hy"][j] = new Float32Array(ysize);
 };
 
 /*
@@ -66,7 +41,7 @@ DRBM.prototype.normalizeConstant = function() {
 DRBM.prototype.normalizeConstantDiv2H = function() {
     var value = 0.0;
     for (var k = 0; k < this.ysize; k++) {
-        var k_val = Math.exp(this.getBias("y", k));
+        var k_val = Math.exp(this.bias["y"][k]);
         for (var j = 0; j < this.hsize; j++) {
             k_val += Math.cosh(this.muJK(j, k));
         }
@@ -76,10 +51,9 @@ DRBM.prototype.normalizeConstantDiv2H = function() {
 };
 
 DRBM.prototype.muJK = function(hindex, yindex) {
-    var value = this.getBias("h", hindex) + this.getWeight("hy", hindex,
-        yindex);
+    var value = this.bias["h"][hindex] + this.weight["hy"][hindex][yindex];
     for (var i = 0; i < this.xsize; i++) {
-        value += this.getWeight("xh", i, hindex) * this.getNode("x", i);
+        value += this.weight["xh"][i][hindex] * this.node["x"][i];
     }
     return value;
 };
@@ -93,11 +67,11 @@ DRBM.prototype.condProbY = function(yindex) {
 DRBM.prototype.condProbYGivenZ = function(yindex) {
     var z_k = this.normalizeConstantDiv2H();
     var potential = 0.0; {
-        var k_val = Math.exp(this.getBias("y", yindex));
+        var k_val = Math.exp(this.bias["y"][k]);
         for (var j = 0; j < this.hsize; j++) {
-            var mu_j = this.getBias("h", j) + this.getWeight("hy", j, yindex);
+            var mu_j = this.bias["h"][j] * this.weight["hy"][j][yindex];
             for (var i = 0; i < this.xsize; i++) {
-                mu_j += this.getWeight("xh", i, j);
+                mu_j += this.weight["xh"][i][j];
             }
             k_val += Math.cosh(mu_j);
         }
@@ -109,12 +83,12 @@ DRBM.prototype.condProbYGivenZ = function(yindex) {
 
 DRBM.prototype.expectedValueXH = function(xindex, hindex) {
     var z = this.normalizeConstantDiv2H();
-    var value = this.getNode("x", xindex) * this.expectedValueHGivenZ(hindex, z);
+    var value = this.node["x"][xindex] * this.expectedValueHGivenZ(hindex, z);
     return value;
 };
 
 DRBM.prototype.expectedValueXHGivenZ = function(xindex, hindex, z) {
-    var value = this.getNode("x", xindex) * this.expectedValueHGivenZ(hindex, z);
+    var value = this.node["x"][xindex] * this.expectedValueHGivenZ(hindex, z);
     return value;
 };
 
@@ -131,7 +105,7 @@ DRBM.prototype.expectedValueHGivenZ = function(hindex, z) {
     lindex.splice(hindex, 1);
     var value = 0.0;
     for (var k = 0; k < this.ysize; k++) {
-        var k_val = Math.exp(this.getBias("y", k));
+        var k_val = Math.exp(this.bias["y"][k]);
         for (var l of lindex) {
             k_val *= Math.cosh(this.muJK(l, k));
         }
@@ -153,7 +127,7 @@ DRBM.prototype.expectedValueHYGivenZ = function(hindex, yindex, z) {
         length: this.hsize
     }, (v, k) => k);
     lindex.splice(hindex, 1);
-    var value = Math.exp(this.getBias("y", yindex));
+    var value = Math.exp(this.bias["y"][yindex]);
     for (var l of lindex) {
         value *= Math.cosh(this.muJK(l, yindex));
     }
@@ -172,7 +146,7 @@ DRBM.prototype.expectedValueYGivenZ = function(yindex, z) {
     var lindex = Array.from({
         length: this.hsize
     }, (v, k) => k);
-    var value = Math.exp(this.getBias("y", yindex));
+    var value = Math.exp(this.bias["y"][yindex]);
     for (var l of lindex) {
         value *= Math.cosh(this.muJK(l, yindex));
     }
@@ -185,32 +159,13 @@ function DRBMTrainer(drbm) {
     this.gradientBias["h"] = new Float32Array(drbm.hsize);
     this.gradientBias["y"] = new Float32Array(drbm.ysize);
     this.gradientWeight = {};
-    this.gradientWeight["xh"] = new Float32Array(drbm.xsize * drbm.hsize);
-    this.gradientWeight["hy"] = new Float32Array(drbm.hsize * drbm.ysize);
-    this.weightLength = {};
-    this.weightLength["xh"] = [drbm.xsize, drbm.hsize];
-    this.weightLength["hy"] = [drbm.hsize, drbm.ysize];
+    this.gradientWeight["xh"] = new Array(drbm.xsize);
+    for(var i = 0; i < gradientWeight["xh"].length; i++)
+        this.gradientWeight["xh"][i] = new Float32Array(drbm.hsize);
+    this.gradientWeight["hy"] = new Array(drbm.hsize);
+    for(var j = 0; j < gradientWeight["hy"].length; j++)
+        this.gradientWeight["hy"][j] = new Float32Array(drbm.ysize);
     this.optimizer = new DRBMOptimizer(drbm);
-};
-
-DRBMTrainer.prototype.getGradientBias = function(name, index) {
-    var value = this.gradientBias[name][index];
-    return value;
-};
-
-DRBMTrainer.prototype.setGradientBias = function(name, index, value) {
-    this.gradientBias[name][index] = value;
-};
-
-DRBMTrainer.prototype.getGradientWeight = function(name, i, j) {
-    var index = i * this.weightLength[name][1] + j;
-    var value = this.gradientWeight[name][index];
-    return value;
-};
-
-DRBMTrainer.prototype.setGradientWeight = function(name, i, j, value) {
-    var index = i * this.weightLength[name][1] + j;
-    this.gradientWeight[name][index] = value;
 };
 
 /*
@@ -226,52 +181,52 @@ DRBMTrainer.prototype.train = function(drbm, data, learning_rate) {
     for (var i = 0; i < drbm.xsize; i++) {
         for (var j = 0; j < drbm.hsize; j++) {
             var gradient = this.dataMeanXH(drbm, data, i, j) - drbm.expectedValueXHGivenZ(i, j, z);
-            this.setGradientWeight("xh", i, j, gradient);
+            this.gradientWeight["xh"][i][j] = gradient;
         }
     }
     for (var j = 0; j < drbm.hsize; j++) {
         var gradient = this.dataMeanH(drbm, data, j) - drbm.expectedValueHGivenZ(j, z);
-            this.setGradientBias("h", j, gradient);
+            this.gradientBias["h"][j] = gradient;
     }
     for (var j = 0; j < drbm.hsize; j++) {
         for (var k = 0; k < drbm.ysize; k++) {
             var gradient = this.dataMeanHY(drbm, data, j, k) - drbm.expectedValueHYGivenZ(j, k, z);
-            this.setGradientWeight("hy", j, k, gradient);
+            this.gradientWeight["hy"][j][k] = gradient;
         }
     }
     for (var k = 0; k < drbm.ysize; k++) {
         var gradient = this.dataMeanY(drbm, data, k) - drbm.expectedValueYGivenZ(k, z);
-        this.setGradientBias("y", k, gradient);
+        this.gradientBias["y"][k] = gradient;
     }
     
     // update
    for (var i = 0; i < drbm.xsize; i++) {
         for (var j = 0; j < drbm.hsize; j++) {
-            var gradient = this.getGradientWeight("xh", i, j);
+            var gradient = this.gradientWeight["xh"][i][j];
             var delta = this.optimizer.deltaWeight("xh", i, j, gradient);
-            var new_param = drbm.getWeight("xh", i, j) + delta;
-            drbm.setWeight("xh", i, j, new_param);
+            var new_param = drbm.weight["xh"][i][j] + delta;
+            drbm.weight["xh"][i][j] = new_param;
         }
     }
     for (var j = 0; j < drbm.hsize; j++) {
-        var gradient = this.getGradientBias("h", j);
+        var gradient = this.gradientBias["h"][j];
         var delta = this.optimizer.deltaBias("h", j, gradient);
-        var new_param = drbm.getBias("h", j) + delta;
-        drbm.setBias("h", j, new_param);
+        var new_param = drbm.bias["h"][j] + delta;
+        drbm.bias["h"][j] = new_param;
     }
     for (var j = 0; j < drbm.hsize; j++) {
         for (var k = 0; k < drbm.ysize; k++) {
-            var gradient = this.getGradientWeight("hy", j, k);
+            var gradient = this.gradientWeight["hy"][j][k];
             var delta = this.optimizer.deltaWeight("hy", j, k, gradient);
-            var new_param = drbm.getWeight("hy", j, k) + delta;
-            drbm.setWeight("hy", j, k, new_param);
+            var new_param = drbm.weight["hy"][j][k] + delta;
+            drbm.weight["hy"][j][k] = new_param;
         }
     }
     for (var k = 0; k < drbm.ysize; k++) {
-        var gradient = this.getGradientBias("y", k);
+        var gradient = this.gradientBias["y"][k];
         var delta = this.optimizer.deltaBias("y", k, gradient);
-        var new_param = drbm.getBias("y", k) + delta;
-        drbm.setBias("y", k, new_param);
+        var new_param = drbm.bias["y"][k] + delta;
+        drbm.bias["y"][k] = new_param;
     }
     
     // update optimizer
@@ -279,18 +234,18 @@ DRBMTrainer.prototype.train = function(drbm, data, learning_rate) {
  };
 
 DRBMTrainer.prototype.dataMeanXH = function(drbm, data, xindex, hindex) {
-    var mu = drbm.getBias("h", hindex) + drbm.getWeight("hy", hindex, data.y);
+    var mu = drbm.bias["h"][hindex] + drbm.weight["hy"][hindex][data.y];
     for (var i = 0; i < drbm.xsize; i++) {
-        mu += drbm.getWeight("xh", i, hindex) * data.x[i];
+        mu += drbm.weight["xh"][i][hindex] * data.x[i];
     }
     var value = data.x[xindex] * Math.tanh(mu);
     return value;
 };
 
 DRBMTrainer.prototype.dataMeanH = function(drbm, data, hindex) {
-    var mu = drbm.getBias("h", hindex) + drbm.getWeight("hy", hindex, data.y);
+    var mu = drbm.bias["h"][hindex] + drbm.weight["hy"][hindex][data.y];
     for (var i = 0; i < drbm.xsize; i++) {
-        mu += drbm.getWeight("xh", i, hindex) * data.x[i];
+        mu += drbm.weight["xh"][i][hindex] * data.x[i];
     }
     var value = Math.tanh(mu);
     return value;
@@ -298,9 +253,9 @@ DRBMTrainer.prototype.dataMeanH = function(drbm, data, hindex) {
 
 DRBMTrainer.prototype.dataMeanHY = function(drbm, data, hindex, yindex) {
     if (yindex !== data.y) return 0.0;
-    var mu = drbm.getBias("h", hindex) + drbm.getWeight("hy", hindex, data.y);
+    var mu = drbm.bias["h"][hindex] + drbm.weight["hy"][hindex][data.y];
     for (var i = 0; i < drbm.xsize; i++) {
-        mu += drbm.getWeight("xh", i, hindex) * data.x[i];
+        mu += drbm.weight["xh"][i][hindex] * data.x[i];
     }
     var value = Math.tanh(mu);
     return value;
@@ -324,17 +279,22 @@ function DRBMOptimizer(drbm) {
     this.momentBias1["h"] = new Float32Array(drbm.hsize);
     this.momentBias1["y"] = new Float32Array(drbm.ysize);
     this.momentWeight1 = {};
-    this.momentWeight1["xh"] = new Float32Array(drbm.xsize * drbm.hsize);
-    this.momentWeight1["hy"] = new Float32Array(drbm.hsize * drbm.ysize);
+    this.momentWeight1["xh"] = new Array(drbm.xsize);
+    for(var i = 0; i < this.momentWeight1["xh"].length; i++)
+         this.momentWeight1["xh"][i] = new Float32Array(drbm.hsize);
+    this.momentWeight1["hy"] = new Array(drbm.xsize);
+    for(var j = 0; j < this.momentWeight1["hy"].length; j++)
+         this.momentWeight1["hy"][j] = new Float32Array(drbm.hsize);
     this.momentBias2 = {};
     this.momentBias2["h"] = new Float32Array(drbm.hsize);
     this.momentBias2["y"] = new Float32Array(drbm.ysize);
     this.momentWeight2 = {};
-    this.momentWeight2["xh"] = new Float32Array(drbm.xsize * drbm.hsize);
-    this.momentWeight2["hy"] = new Float32Array(drbm.hsize * drbm.ysize);
-    this.weightLength = {};
-    this.weightLength["xh"] = [drbm.xsize, drbm.hsize];
-    this.weightLength["hy"] = [drbm.hsize, drbm.ysize];
+    this.momentWeight2["xh"] = new Array(drbm.xsize);
+    for(var i = 0; i < this.momentWeight2["xh"].length; i++)
+         this.momentWeight2["xh"][i] = new Float32Array(drbm.hsize);
+    this.momentWeight2["hy"] = new Array(drbm.xsize);
+    for(var j = 0; j < this.momentWeight2["hy"].length; j++)
+         this.momentWeight2["hy"][j] = new Float32Array(drbm.hsize);
 };
 
 DRBMOptimizer.prototype.deltaBias = function(name, index, gradient) {
@@ -345,9 +305,8 @@ DRBMOptimizer.prototype.deltaBias = function(name, index, gradient) {
 };
 
 DRBMOptimizer.prototype.deltaWeight = function(name, i, j, gradient) {
-    var index = i * this.weightLength[name][1] + j;
-    var m = this.momentWeight1[name][index] = this.beta1 * this.momentWeight1[name][index] + (1.0 - this.beta1) * gradient;
-    var v = this.momentWeight2[name][index] = Math.max(this.beta2 * this.momentWeight2[name][index], Math.abs(gradient));
+    var m = this.momentWeight1[name][j] = this.beta1 * this.momentWeight1[name][j] + (1.0 - this.beta1) * gradient;
+    var v = this.momentWeight2[name][j] = Math.max(this.beta2 * this.momentWeight2[name][j], Math.abs(gradient));
     var delta = this.alpha / (1.0 - Math.pow(this.beta1, this.iteration)) * m / (v + this.epsilon);
     return delta;
 };
